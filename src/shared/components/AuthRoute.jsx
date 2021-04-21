45a9f7ea3c5b0dbcdaff716a0e24ee7a06832ad1
Mikhail Volodin <volodinmiisha1@gmail.com>
2021-04-21 19:58:21 +0300

    LogoutBtnProfile
    - Add Logout button
    - Add something like profile
    - Add Auth button

--- src/shared/components/AuthRoute.jsx
+++ src/shared/components/AuthRoute.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 const AuthRoute = ({ children }) => {
-  const { loggedIn } = useSelector(state => state.auth);
-  const history = useHistory();
+	const { loggedIn } = useSelector((state) => state.auth);
+	const history = useHistory();
 
-  useEffect(() => {
-    if (loggedIn) {
-      history.replace('/');
-    } else {
-      history.push('/auth');
-    }
-  }, [loggedIn, history.location.pathname]);
+	useEffect(
+		() => {
+			if (loggedIn) {
+				history.replace('/');
+			} else {
+				history.push('/auth');
+			}
+		}, // eslint-disable-next-line react-hooks/exhaustive-deps
+		[ loggedIn, history.location.pathname ]
+	);
 
-  return children;
+	return children;
 };
 
 export default AuthRoute;
--- src/store/features/auth.js
+++ src/store/features/auth.js
@@ -2,70 +2,78 @@ import { createSlice } from '@reduxjs/toolkit';
 import { request } from '../../shared/utils/request';
 
 const initialState = {
-  isLoading: false,
-  error: '',
-  data: null,
-  loggedIn: !!localStorage.getItem('auth_token'),
-  user: null,
+	isLoading: false,
+	error: '',
+	data: null,
+	user: null,
+	loggedIn: !!localStorage.getItem('auth_token')
 };
 
 export const counterSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setLoading: (state, action) => {
-      state.isLoading = action.payload;
-    },
-    setError: (state, action) => {
-      state.error = action.payload;
-    },
-    setData: (state, action) => {
-      state.data = action.payload;
-    },
-    setLoggedIn: (state, action) => {
-      state.loggedIn = action.payload;
-    },
-    setUser: (state, action) => {
-      state.user = action.payload;
-    },
-  },
+	name: 'auth',
+	initialState,
+	reducers: {
+		setLoading: (state, action) => {
+			state.isLoading = action.payload;
+		},
+		setError: (state, action) => {
+			state.error = action.payload;
+		},
+		setData: (state, action) => {
+			state.data = action.payload;
+		},
+		setLoggedIn: (state, action) => {
+			state.loggedIn = action.payload;
+		},
+		setUser: (state, action) => {
+			state.user = action.payload;
+		}
+	}
 });
 
 export const { setError, setData, setLoading, setLoggedIn, setUser } = counterSlice.actions;
 
-// export const login = data => dispatch => {
-//   dispatch(setLoading(true));
+export const signUp = (data) => (dispatch) => {
+	dispatch(setLoading(true));
 
-//   request(`/auth/local/`)
-// }
-
-export const signUp = data => dispatch => {
-  dispatch(setLoading(true));
-
-  request(`/auth/local/register`, 'POST', data)
-    .then(r => {
-      dispatch(setLoggedIn(true));
-      localStorage.setItem('auth_token', r.jwt);
-      dispatch(setUser(r.user));
-    })
-    .catch(e => {
-      dispatch(setError(e));
-    })
-    .finally(() => {
-      dispatch(setLoading(false));
-    });
+	request(`/auth/local/register`, 'POST', data)
+		.then((r) => {
+			dispatch(setLoggedIn(true));
+			localStorage.setItem('auth_token', r.jwt);
+			dispatch(setUser(r.user));
+		})
+		.catch((err) => {
+			dispatch(setError(err.message));
+		})
+		.finally(() => {
+			dispatch(setLoading(false));
+		});
 };
 
-export const logout = data => dispatch => {};
+export const login = (data) => (dispatch) => {
+	dispatch(setLoading(true));
 
-// export const sendingData = data => dispatch => {
-//   dispatch(setLoading(true));
+	request(`/auth/local`, 'POST', {
+    identifier: data.email,
+    password: data.password,
+  })
+		.then((r) => {
+      dispatch(setLoggedIn(true));
+			localStorage.setItem('auth_token', r.jwt);
+			dispatch(setUser(r.user));
+		})
+		.catch((err) => {
+			dispatch(setError(err.message));
+		})
+		.finally(() => {
+			dispatch(setLoading(false));
+		});
+};
 
-//     request(`/auth/local/register`, 'POST', data)
-//       .then(() => dispatch(setSending(true)))
-//       .catch(err => {
-//       dispatch(setError(err.message));
-//       });
-// };
+export const logout = () => (dispatch) => {
+	localStorage.removeItem('auth_token');
+	dispatch(setLoggedIn(false));
+	dispatch(setUser(null));
+};
 
 export default counterSlice.reducer;
